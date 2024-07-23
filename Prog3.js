41ff2c028000f35ef02f6bccda87d62754f30e70
Ryana265 <99040464+Ryana265@users.noreply.github.com>
2024-07-23 10:09:53 +0530

    Create Prog3.js

---
+++ Prog3.js
@@ -0,0 +1,4 @@
+// Print "You can Drive" or "You can't Drive" based on age being greater than 18 using ternary operator
+let age1 = 20; 
+let canDrive = age1 > 18 ? "You can Drive" : "You can't Drive";
+console.log(canDrive);
