e7997520ab22024c1a2695f4d20223359e5df85b
Raina Raj <99040464+Ryana265@users.noreply.github.com>
2024-07-24 20:49:42 +0530

    Create binarysearch.js

---
+++ binarysearch.js
@@ -0,0 +1,18 @@
+function binarySearch(arr,target){
+    let left=0;
+    let right=arr.length-1;
+    while(left<=right){
+        const mid=Math.floor((left+right)/2); //mid calculation
+        if(arr[mid]===target){
+            return mid;
+        }
+        else if(arr[mid]<target){
+            left=mid+1;
+        }
+        else{
+            right=mid-1;
+        }
+    }
+    return -1;
+}
+console.log(binarySearch([4,5,6,7,8,9,10],7));
