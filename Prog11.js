5f238f2bcf582ccc521c55f2002c8693109a0567
Raina Raj <99040464+Ryana265@users.noreply.github.com>
2024-07-25 12:18:45 +0530

    Create Prog11.js

---
+++ Prog11.js
@@ -0,0 +1,6 @@
+//Try to Change  4th character of a given string "Where you able to do it?"
+let str = "Where you able to do it?";
+str = str.substring(0, 3) + 'z' + str.substring(4);
+console.log(str); // Output: Whezre you able to do it?
+
+
